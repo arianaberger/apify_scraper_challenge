088883d294d9d7e26a2830e4afb006d2fdab09d7
Ariana <ariana.berger@gmail.com>
2019-08-12 15:34:29 -0400

    Get some simple node UI tests working

--- test/sample.spec.js
+++ test/sample.spec.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 const { expect } = require('chai');
-const _ = require('lodash');
+const _ = require('lodash'); //JS helper to give us access to some functions
 const globalVariables = _.pick(global, ['browser', 'expect']);
 
 // puppeteer options
@@ -10,22 +10,29 @@ const opts = {
   timeout: 10000
 };
 
+//Simple node testing
+const assert = require('assert');
+const http = require('https');
+const url = 'https://www.visithoustontexas.com/event/zumba-in-the-plaza/59011/';
+
+describe('sample test', function () {
+  it('tests are working!', function () {
+    expect(true).to.be.true;
+  });
+
+  it('should return 200', function (done) {
+    http.get(url, function (res) {
+      assert.equal(200, res.statusCode);
+        done();
+    }
+  )
+});
+
+
 // expose variables
 before (async function () {
   global.expect = expect;
   global.browser = await puppeteer.launch(opts);
 });
 
-describe('sample test', function () {
-  it('should work', function () {
-    expect(true).to.be.true;
-  });
-});
-
-// close browser and reset global variables
-after (function () {
-  browser.close();
-
-  global.browser = globalVariables.browser;
-  global.expect = globalVariables.expect;
 });
